c4d653b63ddc9d508841fa9b915a2664d9e4dbe2
hervin21 <142458280+hervin21@users.noreply.github.com>
2023-10-06 22:26:48 -0600

    Agregar archivos de proyecto.

---
+++ arreglo de aves/arreglo_de_aves.py
@@ -0,0 +1,14 @@
+Aves = ["Loro gris","Paloma de diamante","Coctel"]
+print ("Los valores de la lista antes de inserta:")
+for ave in Aves:
+    print (ave, end= "")
+print("\n")
+
+Aves.append ("Mayna")
+Aves.append ("Periquitos")
+Aves.append ("Cacatua")
+print("Los valores de la lista despues de inserta:")
+
+for ave in Aves:
+    print (ave, end= "")
+print("\n")
